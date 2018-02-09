460143d16e99d97d243dff8d36389273f3304e6c
HenryFBP <HenryFBP@gmail.com>
2018-02-09 13:07:53 -0600

    add pygubu xml

--- gui.py
+++ gui.py
@@ -1,13 +1,20 @@
 from tkinter import *
+import pygubu
 
-root = Tk()
+class Application:
+    def __init__(self, master, path="./gui.ui"):
 
-def quit(e):
-    root.destroy()
+        #1: Create a builder
+        self.builder = builder = pygubu.Builder()
 
-buttonQuit = Button(root, text="exit")
-buttonQuit.pack()
-buttonQuit.bind('<Button-1>', quit)
+        #2: Load an ui file
+        builder.add_from_file(path)
 
-print("Before mainloop")
-root.mainloop()
+        #3: Create the widget using a master as parent
+        self.mainwindow = builder.get_object('mainwindow', master)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    app = Application(root)
+    root.mainloop()
