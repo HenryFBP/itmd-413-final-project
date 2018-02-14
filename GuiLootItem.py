0f23b7f91c78f4b36d1e656ebb18189cae7e3eed
HenryFBP <HenryFBP@gmail.com>
2018-02-14 17:00:58 -0600

    add lootItem classes

---
+++ GuiLootItem.py
@@ -0,0 +1,13 @@
+import LootItem
+
+class GuiLootItem:
+    """
+    A class that exists to turn a LootItem into a tkinter GUI element.
+    """
+
+    item = LootItem
+
+    def __init__(self, item):
+        pass
+
+    def frame(self):
---
+++ LootItem.py
@@ -0,0 +1,17 @@
+class LootItem:
+    """    A class representing a loot item that can come from a loot crate.
+
+    Attributes
+    ----------
+        name : str      The name of the item.
+        rarity : float  The weight to be multiplied into the chance that this item will be selected.
+    """
+
+    name = "Loot Item"
+    rarity = 0.5
+
+    def __init__(self):
+        return
+
+    def __init__(self, name):
+        self.name = name
