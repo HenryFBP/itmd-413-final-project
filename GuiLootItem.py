a06b52cfd7ff9ce86e4796848de889e0ca019b21
Henry Post <HenryFBP@gmail.com>
2018-02-14 19:00:19 -0600

    finish LootItem -> Frame class

--- GuiLootItem.py
+++ GuiLootItem.py
@@ -1,13 +1,21 @@
-import LootItem
+from tkinter import *
+
+from guiLib import *
+
 
 class GuiLootItem:
     """
     A class that exists to turn a LootItem into a tkinter GUI element.
     """
 
-    item = LootItem
+    def item_to_frame(item: LootItem, parent) -> Frame:
+        """
+        :return: ``item`` as a tkinter Frame object.
+        """
 
-    def __init__(self, item):
-        pass
+        frame = Frame(parent, bd=2, relief=SUNKEN, background=("#" + random_hex_string()))
 
-    def frame(self):
+        label = Label(frame, text=item.name)
+        label.pack()
+
+        return frame
