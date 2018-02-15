8bc4469508167711dfdd5bba20de2d978038b4d5
Henry Post <HenryFBP@gmail.com>
2018-02-14 18:59:58 -0600

    finish randomLootItem()

--- guiLib.py
+++ guiLib.py
@@ -1,10 +1,26 @@
 import random
-import LootItem
+
+from LootItem import *
 
 hex_digits = "0123456789ABCDEF"
 
+modifiers = ["bad", "ok", "good", "awesome", "godly"]
+types = ["sword", "gun", "churro", "noodle", "shield"]
+conjs = ["of", "in", "with", "without", "having", "derived from", "inside of", "outside of"]
+attrs = ["justice", "evil", "zits", "pastries", "noodle-osity", "helplessness", "body odor"]
+
+
+def random_item(list):
+    """Return a random item in list ``list`` between ``0`` and ``len(list)``. """
+    return list[random.randrange(0, len(list))]
+
+
 def randomLootItem() -> LootItem:
-    return "potatoes"
+    name = f"{random_item(modifiers)} {random_item(types)} {random_item(conjs)} {random_item(attrs)}"
+    rarity = random.uniform(0, 1)
+
+    return LootItem(name, rarity)
+
 
 def random_hex_digit():
     """
