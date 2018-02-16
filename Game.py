7bf4b5465c1a4c05b4c898d2105ae0bc6d023898
Henry Post <HenryFBP@gmail.com>
2018-02-16 15:09:55 -0600

    add skeleton game-controller class

---
+++ Game.py
@@ -0,0 +1,17 @@
+class Game:
+    """
+    A class that facilitates/organizes the following:
+
+    - Currency
+        - Purchasing/using currency
+
+    - LootCrate objects
+        - Registering LootCrate objects
+        - Deciding what items a LootCrate object will contain
+
+    - Having a master list of all items
+        - Indexing an item by an attribute (name, rarity, etc)
+    """
+
+    def __init__(self):
+        pass
