39fba5de21fa248ea2f3804ea87f80a6dfe72731
Henry Post <HenryFBP@gmail.com>
2018-02-16 16:47:17 -0600

    fix erroneous comment replacing

--- Game.py
+++ Game.py
@@ -7,9 +7,9 @@ class Game:
 
     - LootCrate objects
         - Registering LootCrate objects
-        - Deciding what pool a LootCrate object will contain
+        - Deciding what items a LootCrate object will contain
 
-    - Having a master list of all pool
+    - Having a master list of all items
         - Indexing an item by an attribute (name, rarity, etc)
     """
 
