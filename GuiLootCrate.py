22a762757912d30f8b7edb657a1a6900b2abb12d
HenryFBP <HenryFBP@gmail.com>
2018-03-09 11:30:36 -0600

    add GuiLootCrate

---
+++ GuiLootCrate.py
@@ -0,0 +1,19 @@
+from LootCrate import *
+
+
+class GuiLootCrate:
+    """
+    A class that exists to turn a LootCrate into a tkinter GUI element.
+    """
+
+    def crate_to_frame(crate: LootCrate, parent):
+        """
+        :return: ``crate`` as a ``tkinter`` ``Frame`` object.
+        """
+        rf = Frame(parent, bd=2, relief=SUNKEN)
+
+        name = Label(rf, text=crate.name)
+
+        name.pack()
+
+        return rf
